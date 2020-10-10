711953ded51ad07b0c950378a97304829b1a45cd
Javon Ellis <javon.ellis@aye-tech.com>
2020-10-10 13:18:43 -0500

    Alternate X and O's based on click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,45 @@
+
+
+
 window.onload = function (){
     setSquares();
+    setSquareContent();
 }
 
+
+
+
+//Applies CSS for the squares(Div)
 function setSquares(){
     let board  = document.getElementById('board');
     let squares = board.children;
     for(var i=0; i<squares.length; i++){
         squares[i].setAttribute("class","square");
+    }
+}
+
+
+//set Inner Html based on X or 0
+function setSquareContent(){
+    let board  = document.getElementById('board');
+    let squares = board.children;
+    let finalArr = [];
+    for(var i=0; i<squares.length; i++){
+        let singleSquare = squares[i];
+        if(finalArr.length <8){
+            singleSquare.addEventListener("click",function (){
+                if(finalArr.length == 0 || finalArr[finalArr.length-1] == "O" && finalArr.length<9){
+                    singleSquare.innerHTML = "X";
+                    singleSquare.classList.add("X");
+                    finalArr.push("X");
+                }else if(finalArr[finalArr.length-1] == "X" && finalArr.length<9){
+                    singleSquare.innerHTML = "O";
+                    singleSquare.classList.add("O");
+                    finalArr.push("O");
+
+                }
+            })
+        }
+
     }
 }
