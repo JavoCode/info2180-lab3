5d171f2e34fe354f8579030b3e3dcb1780b98bef
Javon Ellis <javon.ellis@aye-tech.com>
2020-10-11 18:53:50 -0500

    Added winner announcement after deciding winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,14 @@
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
 window.onload = function (){
@@ -24,23 +34,49 @@ function setSquares(){
 function setSquareContent(){
     let board  = document.getElementById('board');
     let squares = board.children;
-    let finalArr = [];
+    let finalArr = ["","","","","","","","",""];
+    let player = "O"
+    let count = 0;
     for(var i=0; i<squares.length; i++){
         let singleSquare = squares[i];
-        if(finalArr.length <8){
             singleSquare.addEventListener("click",function (){
-                if(finalArr.length == 0 || finalArr[finalArr.length-1] == "O" && finalArr.length<9){
+                if(player == "O"){
                     singleSquare.innerHTML = "X";
                     singleSquare.classList.add("X");
-                    finalArr.push("X");
-                }else if(finalArr[finalArr.length-1] == "X" && finalArr.length<9){
+                    finalArr[count] = ("X");
+                    count++;
+                    player = "X"
+
+                    if(count>=5){
+                        console.log(squares)
+                        let gameState = [];
+                        for(let i=0; i<squares.length;i++){
+
+                            gameState.push(squares[i].innerHTML)
+                        }
+                        handleResultValidation(gameState)
+                    }
+
+                    console.log(finalArr);
+                }else if(player == "X"){
                     singleSquare.innerHTML = "O";
                     singleSquare.classList.add("O");
-                    finalArr.push("O");
+                    finalArr[count] = "O"
+                    count++;
+                    player = "O"
+                    if(count>=5){
+                        console.log(squares)
+                        let gameState = [];
+                        for(let i=0; i<squares.length;i++){
+
+                            gameState.push(squares[i].innerHTML)
+                        }
+                        handleResultValidation(gameState)
+                    }
+
 
                 }
             })
-        }
 
     }
 }
@@ -58,5 +94,29 @@ function hoverMouse(){
             singleSquare.classList.remove("hover");
         })
 
+    }
+}
+
+function handleResultValidation(gameState) {
+    let roundWon = false;
+    let winner = "";
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === "" || b === "" || c === "") {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            winner = a;
+            break
+        }
+    }
+    if (roundWon) {
+        let statusDiv = document.getElementById("status");
+        statusDiv.innerHTML = winner + " won"
+        statusDiv.classList.add("you-won");
     }
 }
